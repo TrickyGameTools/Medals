6395cb5ff3ea4dcb3dde8cd605ed6c8c1b914594
Jeroen Broks <facebook@tbbs.nl>
2021-11-22 23:00:29 +0100

    Fake source. The real source is closed sourced, but in order to make sure projects using "Medals" can at least compile I got you this fake file. Make sure the "#define TRUE_MEDALS" line in Medals.cpp is replaced by "#undef TRUE_MEDALS" before you compile though... Prevents you a lot of crap.

---
+++ Medals.cpp
@@ -0,0 +1,39 @@
+
+#include "Medals.hpp"
+
+// When you don't have the TrueMedals.hpp file (which you shouldn't have)
+// Make sure you set this to "#undef TRUE_MEDALS" or the system may go a bit boom-boom!
+#define TRUE_MEDALS
+
+#include <string.h>
+#include <string>
+
+namespace TrickyMedals {
+	char _Storage[100]{ "Store" };
+
+	void Storage(const char* s) {
+		strcpy_s(_Storage, s);
+	}
+
+	std::string Storage() {
+		return _Storage;
+	}
+	
+	void LoadFromJCR6(std::string main, std::string medalfile) { LoadFromJCR6(&jcr6::Dir(main), medalfile); }
+
+}
+
+#ifdef TRUE_MEDALS
+#include "TrueMedals.hpp"
+#include "Medals.hpp"
+#else
+namespace TrickyMedals {
+	void LoadFromInternet(){} // Make sure all points are known
+	int MedalValue(const char* game, const char* medal) { return 0; }
+	int FullScore() { return 0; }
+	void Award(const char* game, const char* medal){}
+	int MaxScore() {}
+	int ScorePercent() {}
+	void LoadFromJCR6(jcr6::JT_Dir* main, std::string medalfile) {}
+}
+#endif
